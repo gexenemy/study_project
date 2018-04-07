bc877443af923f2b301f5acb98050ab5a91a7ba1
Gennadiy Pokidov <faqvsem2@yandex.ru>
2018-04-07 14:53:43 +0700

    скрипт

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -4,9 +4,13 @@ var elements = document.querySelectorAll('*'),
     lengthElements = elements.length;
 
 
+
+
 var randomNumb = function getRandomArbitrary(el) {
   return Math.floor(Math.random() * lengthElements);
 };
+
+els[random].style.background = "rgb(255, 0, 0)";
 
 
 console.log(randomNumb(lengthElements));
@@ -14,7 +18,7 @@ console.log(randomNumb(lengthElements));
 
 // 	random = Math.floor(Math.random() * 100);
 
-// 	els[random].style.background = "rgb(255, 0, 0)";
+//
 
 // 	setInterval(function function_name() {
 // 		els[random].style.background = "red";
