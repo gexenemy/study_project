ae1e70c8106d56b2e220ab8353c16694c99dcffc
Gennadiy Pokidov <faqvsem2@yandex.ru>
2018-04-14 01:22:34 +0700

    слайдер пакетов

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,108 +1,225 @@
-// home_work_js_1
+document.addEventListener("DOMContentLoaded", function () {
 
-function getRandomNumber(upperLimit) {
-  return Math.floor(Math.random() * upperLimit);
-}
+	function addPackage({
+		img,
+		name,
+		date
+	}) {
 
-// function getRandomElement() {
-// 	let randomNumber = getRandomNumber(lengthAllElements);
-// 	return randomNumber;
-// }
+		const template = document.querySelector('#slider-item'),
+				  currentImg = template.content.querySelector('.slider-item__img'),
+				  currentName = template.content.querySelector('.slider-item__link'),
+				  currentDate = template.content.querySelector('.slider-item__date');
 
-// function paintRandomColor() {
-// 	allElements[getRandomElement()].style.backgroundColor = "rgb(".concat(getRandomNumber(256), ", ", getRandomNumber(256), ", ",getRandomNumber(256), ")");
-// }
+		currentImg.style.backgroundImage = 'url(assets/'.concat(img, ')');
+		currentName.innerHTML = name;
+		currentDate.innerHTML = date;
 
-// function deleteRandomColor() {
-// 	allElements[getRandomElement()].style.backgroundColor = null;
-// }
-
-// const allElements = document.querySelectorAll('*'),
-// 		  lengthAllElements = allElements.length,
-// 		  randomElement = allElements[getRandomElement()];
-
-// randomElement.style.backgroundColor = "red";
-
-// setInterval(paintRandomColor, 2000);
-
-// setInterval(deleteRandomColor, 2000);
-
-// home_work_1_end
-
-// home_work_2
-
-document.addEventListener("DOMContentLoaded", function() {
-
-	function createPackage({img, name, date}) {
-
-		let packagesWrap = document.querySelector('.category'),
-				package = document.createElement('div'),
-				packageImg = document.createElement('div'),
-				packageName = document.createElement('a'),
-				packageDate = document.createElement('time');
-
-		package.className = 'category__item item-responsive item-4 fade';
-		packageImg.className = 'category__img';
-		packageName.className = 'category__link';
-		packageDate.className = 'category__date';
-
-		package.appendChild(packageImg);
-		package.appendChild(packageName);
-		package.appendChild(packageDate);
-		packagesWrap.appendChild(package);
-
-		packagesWrap.style.maxHeight = '1000px';
-		packageImg.style.backgroundImage = 'url(assets/'.concat(img, ')') ;
-		packageName.innerHTML = name;
-		packageDate.innerHTML = date;
+		document.querySelector('.sliderBody').appendChild(template.content.cloneNode(true));
 
 	}
 
 
-	function checkAddedPackages() {
+	const packageContent = [
 
-		let parentPackagesLength = document.querySelector('.category').children.length;
+		{
+			img: 'shot-1.jpg',
+			name: 'Стандартный пакет',
+			date: '08 aпреля 2012'
+		},
+		{
+			img: 'shot-2.jpg',
+			name: 'Новый ЦФТ-банк',
+			date: '09 сентября 2016'
+		},
+		{
+			img: 'shot-3.jpg',
+			name: 'Каталог разработок',
+			date: '13 января 2015'
+		},
+		{
+			img: 'shot-4.jpg',
+			name: 'Автоплатежи',
+			date: '23 марта 2015'
+		},
+		{
+			img: 'shot-5.jpg',
+			name: 'Доверительное управление',
+			date: '15 июня 2015'
+		},
+		{
+			img: 'shot-6.jpg',
+			name: 'Выплаты АСВ',
+			date: '17 июля 2015'
+		},
+		{
+			img: 'shot-7.jpg',
+			name: 'Срочные вклады',
+			date: '26 октября 2015'
+		},
+		{
+			img: 'shot-8.jpg',
+			name: 'Пенсионная карта',
+			date: '30 ноября 2015'
+		},
+		{
+			img: 'shot-9.jpg',
+			name: 'Оптимальный',
+			date: '04 августа 2015'
+		},
+		{
+			img: 'shot-10.jpg',
+			name: 'Премиальный 5',
+			date: '19 мая 2015'
+		},
 
-		if (parentPackagesLength > 2) {
-			clearInterval(addPackage);
-			return false;
+	]
+
+
+	function shuffleArray(array) {
+
+		for (let i = array.length - 1; i > 0; i--) {
+
+			let j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+
 		}
 
 	}
 
 
-	function addRandomPackage() {
+	shuffleArray(packageContent);
 
-		let packageContentLength = packageContent.length,
-				randomPackageContent = packageContent[getRandomNumber(packageContentLength)];
 
-		createPackage(randomPackageContent);
+	packageContent.forEach(function (currentPackage) {
+		addPackage(currentPackage);
+	});
 
-		checkAddedPackages();
+
+	function initializeSlider(item, padding) {
+
+		const sliderWidth = document.querySelector('.slider').clientWidth,
+					slider = document.querySelector('.sliderBody'),
+					slides = slider.querySelectorAll('.slider-item'),
+					sliderBodyLength = document.querySelector('.sliderBody').children.length,
+					dotsBody = document.querySelector('.slider-dots'),
+					dotTemplate = document.querySelector('#slider-dot'),
+					dot = dotTemplate.content.querySelector('.slider-dot'),
+					buttonsBody = document.querySelector('.slider__buttons'),
+					button = document.querySelectorAll('.slider__button'),
+
+					slideWidth = parseInt(((sliderWidth + padding) / item).toFixed(2)),
+					sliderBodyWidth = parseInt((slideWidth * sliderBodyLength).toFixed(2)),
+					sliderStage = parseInt((slideWidth * 2 - sliderBodyWidth).toFixed(2));
+
+
+		for (let i = 0; i < slides.length; i++) {
+
+			slides[i].style.width = ''.concat(slideWidth, 'px');
+			slides[i].style.paddingRight = ''.concat(padding, 'px');
+			dot.dataset.index = ''.concat(i, '');
+			dotsBody.appendChild(dotTemplate.content.cloneNode(true));
+
+		}
+
+
+		dotsBody.children[1].classList.add('slider-dot_active');
+		slider.style.width = ''.concat(sliderBodyWidth, 'px');
+
+
+		const dots = document.querySelectorAll('.slider-dot'),
+					dotsBodyLength = dotsBody.children.length;
+
+		let move = 0;
+
+
+		function dotsFunc(currentDot) {
+
+			const activeDot = document.querySelector('.slider-dot_active');
+
+			let indexDot = parseInt(currentDot.dataset.index);
+
+			move = parseInt((-(slideWidth * (indexDot - 1))).toFixed(2));
+
+			activeDot.classList.remove('slider-dot_active');
+			currentDot.classList.add('slider-dot_active');
+			slider.style.left = ''.concat(move, 'px');
+
+		}
+
+
+		function sliderButton(button) {
+
+			const activeDot = document.querySelector('.slider-dot_active');
+			activeDot.classList.remove('slider-dot_active');
+
+
+			if (button.classList.contains('slider__button-left')) {
+
+				move += slideWidth;
+
+				if (activeDot.dataset.index == 0) {
+
+					dots[dotsBodyLength - 1].classList.add('slider-dot_active');
+
+				}
+				else {
+
+					activeDot.previousElementSibling.classList.add('slider-dot_active');
+
+				}
+
+			}
+			else {
+
+				move -= slideWidth;
+
+				if (activeDot.dataset.index == dotsBodyLength - 1) {
+
+					dots[0].classList.add('slider-dot_active');
+
+				}
+				else {
+
+					activeDot.nextElementSibling.classList.add('slider-dot_active');
+
+				}
+
+			}
+
+
+			if (move > slideWidth) {
+				move = sliderStage;
+			}
+			else if (move < sliderStage) {
+				move = slideWidth;
+			}
+
+
+			slider.style.left = ''.concat(move, 'px');
+
+
+		}
+
+
+		dotsBody.addEventListener('click', function (event) {
+			const target = event.target;
+			if (target.className == 'slider-dot') {
+				dotsFunc(target);
+			}
+		});
+
+		buttonsBody.addEventListener('click', function (event) {
+			const target = event.target;
+			if (event.isTrusted) {
+				if (target.classList.contains('slider__button')) {
+					sliderButton(target);
+				}
+			}
+		});
 
 	}
 
-
-	let packageContent = [
-
-		{img: 'shot-1.jpg', name: 'Стандартный пакет', date: '08 aпреля 2012'},
-		{img: 'shot-2.jpg', name: 'Новый ЦФТ-банк', date: '09 сентября 2016'},
-		{img: 'shot-3.jpg', name: 'Каталог разработок', date: '13 января 2015'},
-		{img: 'shot-4.jpg', name: 'Автоплатежи', date: '23 марта 2015'},
-		{img: 'shot-5.jpg', name: 'Доверительное управление', date: '15 июня 2015'},
-		{img: 'shot-6.jpg', name: 'Выплаты АСВ', date: '17 июля 2015'},
-		{img: 'shot-7.jpg', name: 'Срочные вклады', date: '26 октября 2015'},
-		{img: 'shot-8.jpg', name: 'Пенсионная карта', date: '30 ноября 2015'},
-		{img: 'shot-9.jpg', name: 'Оптимальный', date: '04 августа 2015'},
-		{img: 'shot-10.jpg', name: 'Премиальный 5', date: '19 мая 2015'},
-
-	]
-
-
-	let addPackage = setInterval(addRandomPackage, 500);
+	initializeSlider(3, 20);
 
 });
-
-// home_work_2_end
-
-
